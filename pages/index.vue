fa18e19c36c8a85db8801acdfd6346e35ecb0da1
Indie Breath <indiebreath@proton.me>
2024-04-09 13:46:07 +1000

    Homepage structure complete, no CSS

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="page">
+    <div id="info-wrapper">
+      <img src="" alt="" />
+      <h1>indiebreath</h1>
+      <p>Melbourne, Australia</p>
+      <ul id="media-links">
+        <li><a href="https://github.com/indie-breath"><img src="" alt="" width="35px" /></a></li>
+      </ul>
+    </div>
+
+    <div id="main">
+      <div id="projects">
+        <h2>Projects</h2>
+        <div id="projects-list">
+          
+          <div className="project-content">
+            <h3>Extant Project</h3>
+            <p>An ARG being told through the medium of a website. </p>
+            <a href="https://github.com/indie-breath/extant-project">https://github.com/indie-breath/extant-project</a>
+          </div>
+          <div className="project-content">
+            <h3>Portfolio Website</h3>
+            <p>My personal portfolio website.</p>
+            <a href="https://github.com/indie-breath/portfolio-website">https://github.com/indie-breath/portfolio-website</a>
+          </div>
+
+        </div>
+      </div>
+
+      <div id="blog">
+        <h2>Blog</h2>
+        <div id="blog-list">
+
+          <p>None for now, check back later.</p> 
+
+        </div>
+      </div>
+    </div>
+
+    <div id="footer">
+      <p>Icons by <a href="icons8.come">Icons8</a></p>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
